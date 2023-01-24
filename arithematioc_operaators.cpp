279703765569f933ada7c27d63c69d1fbab61287
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-01-24 16:25:39 +0530

    arithematic operators

---
+++ arithematioc_operaators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int students =20;
+     int remainder;
+
+    remainder = students%2;
+
+    cout << remainder;
+
+
+    
+
+ 
+   return 0;
+}
